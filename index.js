ae4b60a2a8b39c076d5af4abf069148805fc9e26
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-12 12:26:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var recipes = {
 
 function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object, {key: value});
-  
+  return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
