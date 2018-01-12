c31d37d9f88d052d2902bd124dc2360e9598dcfc
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-12 12:14:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {
+  flour: '2 cups',
+  eggs: '4',
+  sugar: '1 cup'
+};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({}, object, {key: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+    
+}
+
+function destructivelyDeleteFromObjectByKey(object, key, value) {
+  delete object.key
+}
